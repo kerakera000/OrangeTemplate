3a72f50242aa26af0614b07f583e67d3cf415202
lopez <leon51414@icloud.com>
2022-11-16 16:50:06 +0900

    local用のテンプレートを入れた

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="format-detection" content="telephone=no">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		
+		<link rel="stylesheet" href="<?php echo get_theme_file_uri('css/stylesp.css'); ?>">
+		<link rel="stylesheet" href="<?php echo get_theme_file_uri('css/stylepc.css'); ?>">
+		<link rel="stylesheet" href="style.css">
+    <?php wp_head(); ?>
+	</head>
+
+	<body class="body-toggle" id="body" <?php body_class(); ?>>
+
+
+	<body <?php body_class(); ?>>
